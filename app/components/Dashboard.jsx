fe7997790f97fe28ec33d8e902dbc1a0db0755d9
akkinet <akashsharma.zak@gmail.com>
2025-01-04 14:11:12 +0530

    vendor session set

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -4,6 +4,7 @@ import { useSession } from "next-auth/react";
 
 export default function Dashboard() {
   const { data: session } = useSession();
+  console.log("session", session)
 
   return (
     <div className="h-full flex flex-col items-center justify-center bg-gray-100 ">
