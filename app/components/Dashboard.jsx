505bf2546fc59a59f40efb464ae539f91e131598
akkinet <akashsharma.zak@gmail.com>
2025-01-05 10:57:12 +0530

    session fix on dashboard

--- app/components/Dashboard.jsx
+++ app/components/Dashboard.jsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 
-export default function Dashboard() {
-  const { data: session } = useSession();
-  console.log("session", session)
+export default function Dashboard({session}) {
+  // const { data: session } = useSession();
+  // console.log("session", session)
 
   return (
     <div className="h-full flex flex-col items-center justify-center bg-gray-100 ">
@@ -17,28 +17,6 @@ export default function Dashboard() {
             ? `Hello, ${session.user.name}!`
             : "Sign in to personalize your experience."}
         </p>
-
-        <div className="mt-8">
-          {session ? (
-            <button
-              onClick={() => {
-                alert("Access your tools and data!");
-              }}
-              className="bg-customPink text-white px-6 py-2 rounded shadow hover:bg-customBlue transition duration-300"
-            >
-              Go to Tools
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                alert("Redirecting to login...");
-              }}
-              className="bg-blue-600 text-white px-6 py-2 rounded shadow hover:bg-blue-800 transition duration-300"
-            >
-              Sign In
-            </button>
-          )}
-        </div>
       </div>
     </div>
   );
