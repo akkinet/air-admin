54813b83b45942c84129db34fc5903195cbe48cb
akkinet <akashsharma.zak@gmail.com>
2025-03-19 11:51:16 +0530

    booking api

--- app/api/booking/route.js
+++ app/api/booking/route.js
@@ -1,14 +1,18 @@
-import Booking from "@/app/models/Booking";
-import { connectToDatabase } from "@/config/mongo";
+import db from "@/lib/mongodb";
 import { NextResponse } from "next/server";
 
-export const GET = async req => {
-  try{
-    await connectToDatabase();
-    const allBooking = await Booking.find();
+export const GET = async (req) => {
+  try {
+    const { searchParams } = new URL(req.url);
+    const status = searchParams.get("status");
+    const Booking = db.collection("bookings");
+    const filter = {};
+    if (status) filter.status = status;
+
+    const allBooking = await Booking.find(filter).toArray();
     return NextResponse.json(allBooking);
-  }catch(err){
+  } catch (err) {
     console.error("error", err.message);
-    return NextResponse.json({error: err.message}, {status: 500})
+    return NextResponse.json({ error: err.message }, { status: 500 });
   }
-}
+};
